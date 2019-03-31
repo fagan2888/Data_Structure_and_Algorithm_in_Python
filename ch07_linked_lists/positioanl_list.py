df69123c1b98357ee31cf179e515454d19d75764
luoshao23 <luoshao23@gmail.com>
2019-03-31 20:21:00 +0800

    p

--- ch07_linked_lists/positioanl_list.py
+++ ch07_linked_lists/positioanl_list.py
@@ -81,9 +81,31 @@ class PositionalList(_DoublyLinkedBase):
         original._element = e
         return old_value
 
+
+def insertion_sort(L):
+    if len(L) > 1:
+        marker = L.first()
+        while marker != L.last():
+            pivot = L.after(marker)
+            value = pivot.element()
+            if value > marker.element():
+                marker = pivot
+            else:
+                walk = marker
+                while walk != L.first() and L.before(walk).element() > value:
+                    walk = L.before(walk)
+                L.delete(pivot)
+                L.add_before(walk, value)
+
+
 if __name__ == "__main__":
     pl = PositionalList()
-    a1 = pl.add_first(4)
-    a2 = pl.add_after(a1, 5)
-    pl.add_before(a2, 99)
+    a = pl.add_first(4)
+    a = pl.add_after(a, 5)
+    a = pl.add_before(a, 99)
+    a = pl.add_before(a, 32)
+    a = pl.add_before(a, 1)
+    a = pl.add_before(a, 23)
+    print(pl)
+    insertion_sort(pl)
     print(pl)
