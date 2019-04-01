fd9b4721733bffddb564d3ec8be6f10d93876d0a
luoshao23 <luoshao23@gmail.com>
2019-04-02 00:17:08 +0800

    favorites_list

---
+++ ch07_linked_lists/favorites_list_MTF.py
@@ -0,0 +1,27 @@
+from favorites_list import FavoritesList
+from positioanl_list import PositionalList
+
+class FavoritesListMTF(FavoritesList):
+
+    def _move_up(self, p):
+        """Move accessed item at Position p to front of list."""
+        if p != self._data.first():
+            self._data.add_first(self._data.delete(p))
+
+    def top(self, k):
+        if not 1 <= k <= len(self):
+            raise ValueError('Illegal value for k')
+
+        temp = PositionalList()
+        for item in self._data:
+            temp.add_last(item)
+
+        for j in range(k):
+            highPos = temp.first()
+            walk = temp.after(highPos)
+            while walk is not None:
+                if walk.element()._count > highPos.element()._count:
+                    highPos = walk
+                walk = temp.after(walk)
+            yield highPos.element()._value
+            temp.delete(highPos)
