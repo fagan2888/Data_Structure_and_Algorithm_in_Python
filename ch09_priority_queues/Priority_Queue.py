2972d26635e1dc2ae281d8988d0cf744ee60f3c9
luoshao23 <luoshao23@gmail.com>
2019-05-15 09:58:09 +0800

    priority queue

---
+++ ch09_priority_queues/Priority_Queue.py
@@ -0,0 +1,101 @@
+from positioanl_list import PositionalList
+
+
+class PriorityQueueBase:
+    class _Item:
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __lt__(self, other):
+            """compare items based on their keys"""
+            return self._key < other._key
+
+    def is_empty(self):
+        return len(self) == 0
+
+
+class UnsortedPriorityQueue(PriorityQueueBase):
+
+    def _find_min(self):
+        if self.is_empty():
+            raise Empty('Priority queue is empty')
+        small = self._data.first()
+        walk = self._data.after(small)
+        while walk is not None:
+            if walk.element() < small.element():
+                small = walk
+            walk = self._data.after(walk)
+        return small
+
+    def __init__(self):
+        self._data = PositionalList()
+
+    def __len__(self):
+        return len(self._data)
+
+    def add(self, key, value):
+        self._data.add_last(self._Item(key, value))
+
+    def min(self):
+        p = self._find_min()
+        item = p.element()
+        return (item._key, item._value)
+
+    def remove_min(self):
+        p = self._find_min()
+        item = self._data.delete(p)
+        return (item._key, item._value)
+
+
+class SortedPriorityQueue(PriorityQueueBase):
+    def __init__(self):
+        self._data = PositionalList()
+
+    def __len__(self):
+        return len(self._data)
+
+    def add(self, key, value):
+        newest = self._Item(key, value)
+        walk = self._data.last()
+        while walk is not None and newest < walk.element():
+            walk = self._data.before(walk)
+        if walk is None:
+            self._data.add_first(newest)
+        else:
+            self._data.add_after(walk, newest)
+
+    def min(self):
+        if self.is_empty():
+            raise Empty('Priority queue is empty.')
+        p = self._data.first()
+        item = p.element()
+        return (item._key, item._value)
+
+    def remove_min(self):
+        if self.is_empty():
+            raise Empty('Priority queue is empty.')
+        item = self._data.delete(self._data.first())
+        return (item._key, item._value)
+
+
+class Empty(Exception):
+    pass
+
+
+if __name__ == "__main__":
+    P = UnsortedPriorityQueue()
+    P.add(5, 'A')
+    P.add(9, 'C')
+    P.add(3, 'B')
+    P.add(7, 'D')
+    print(P.min())
+    print(P.remove_min())
+    print(P.remove_min())
+    print(len(P))
+    print(P.remove_min())
+    print(P.remove_min())
+    print(P.is_empty())
+    P.remove_min()
